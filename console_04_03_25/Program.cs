4567d16554acd8dbebc3f6aa06769bf4aa6a2856
artemgon <artemhoncharuk123@gmail.com>
2025-03-16 11:23:32 +0200

    Add project files.

---
+++ console_04_03_25/Program.cs
@@ -0,0 +1,47 @@
+﻿using data_04_03_25.Source.LocalDB;
+using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace console_04_03_25
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            try
+            {
+                string connectionString = "Data Source=DESKTOP-F044K8I\\SQLEXPRESS;Initial Catalog=Students;Integrated Security=True;Trust Server Certificate=True";
+                SqlDataProvider dataProvider = new(connectionString);
+                Console.Write("Connection is open: ");
+                Console.WriteLine(dataProvider.IsConnectionOpen());
+                //Console.WriteLine("Executing query...");
+                //dataProvider.VoidExecute("DROP DATABASE TestDB");
+                //Console.WriteLine("Query executed successfully.");
+                Console.WriteLine("Executing query...");
+                dataProvider.VoidExecute("USE Students");
+                string? query;
+                Console.Write("Enter query: ");
+                query = Console.ReadLine();
+                Dictionary<int, List<string?>> result = dataProvider.ReaderExecute(query);
+                foreach (var item in result)
+                {
+                    foreach (var value in item.Value)
+                    {
+                        Console.Write($" {value} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ data_04_03_25/Source/LocalDB/SQLDataProvider.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+
+namespace data_04_03_25.Source.LocalDB
+{
+    public class SqlDataProvider
+    {
+        public string? ConnectionString { get; set; }
+
+        public SqlDataProvider()
+        {
+        }
+
+        public SqlDataProvider(string? connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        public bool IsConnectionOpen()
+        {
+            using SqlConnection connection = new(ConnectionString);
+            connection.Open();
+            return connection.State == ConnectionState.Open;    
+        }
+
+        public void VoidExecute(string query)
+        {
+            try
+            {
+                using SqlConnection connection = new(ConnectionString);
+                connection.Open();
+                using SqlCommand command = new(query, connection);
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public int ScalarExecute(string query)
+        {
+            try
+            {
+                using SqlConnection connection = new(ConnectionString);
+                connection.Open();
+                using SqlCommand command = new(query, connection);
+                return (int)command.ExecuteScalar();
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public Dictionary<int, List<string?>> ReaderExecute(string? query)
+        {
+            if (query == null) throw new Exception("Query is null");
+            Dictionary<int, List<string?>> result = [];
+            try
+            {
+                using SqlConnection connection = new(ConnectionString);
+                connection.Open();
+                using SqlCommand command = new(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    List<string?> row = [];
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        row.Add(reader[i].ToString());
+                    }
+                    result.Add(result.Count, row);
+                }
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+            return result;
+        }
+    }
+}
